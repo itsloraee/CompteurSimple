e455d9a5ad5ade51f463bd8b72090784d602c88d
Loraee <laurxee@gmail.com>
2026-01-09 11:39:38 -0400

    L'émulateur affiche +1

---
+++ components/Counter.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+export default function Counter() {
+const [count, setCount] = useState(0);
+
+return (
+    <View style={styles.container}>
+        <Text style={styles.number}>{count}</Text>
+        <TouchableOpacity style={styles.button}
+        onPress={()=> setCount(count+1)}
+        >
+        <Text style={styles.buttonText}>Ajouter +1 </Text>
+        </TouchableOpacity>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1, 
+        justifyContent:'center',
+        alignItems: 'center',
+    },
+    
+    number: {
+        fontSize: 60,
+        fontWeight: 'bold',
+        marginBottom: 30,
+    },
+
+    button: {
+        backgroundColor: '#007bff',
+        padding: 15,
+        margin: 10,
+        borderRadius:8,   
+    },
+
+    buttonText: {
+        color:'white',
+        fontSize:18
+    },   
+    });
